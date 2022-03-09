eda85aaa5bc0358f53e4506d77f3b2ea2a2579d4
Nitikesh0922 <jerrycreation2204@gmail.com>
2022-03-09 20:46:50 +0530

    day25 completed

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: unnecessary_null_comparison
 
+import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/models/catalog.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartModel {
   //catalog fields
@@ -34,5 +36,15 @@ class CartModel {
   // Remove Items
   void remove(Item item) {
     _itemIds.remove(item.id);
+  }
+}
+
+class AddMutations extends VxMutation<MyStore> {
+  final Item item;
+
+  AddMutations(this.item);
+  @override
+  perform() {
+    store?.cart._itemIds.add(item.id);
   }
 }
--- lib/widgets/home_widgets/add_to_cart.dart
+++ lib/widgets/home_widgets/add_to_cart.dart
@@ -2,29 +2,27 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/models/cart.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class AddToCart extends StatelessWidget {
   final Item catalog;
-  AddToCart({
+  const AddToCart({
     Key? key,
     required this.catalog,
   }) : super(key: key);
 
-  final _cart = CartModel();
   @override
   Widget build(BuildContext context) {
-    bool isInCart = _cart.items.contains(catalog);
+    VxState.watch(context, on: [AddMutations]);
+    final CartModel _cart = (VxState.store as MyStore).cart;
+    bool isInCart = _cart.items.contains(catalog) ?? false;
     return ElevatedButton(
       onPressed: () {
         if (!isInCart) {
-          isInCart = isInCart.toggle();
-          final _catalog = CatalogModel();
-
-          _cart.catalog = _catalog;
-          _cart.add(catalog);
+          AddMutations(catalog);
         }
       },
       style: ButtonStyle(
